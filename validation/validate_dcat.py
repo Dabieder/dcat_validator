afa2ffc43969c570b87ff8bd379a2a0afc4acfd8
Preuß, Nils <nils.preuss@fst.tu-darmstadt.de>
2024-09-04 12:19:41 +0200

    implement validate_dcat with basic functionality

--- validation/validate_dcat.py
+++ validation/validate_dcat.py
@@ -1,4 +1,27 @@
-def validate_rdf(rdf_data: str, shape_file: str):
-    
+import os
+from pyshacl import validate
 
-    return True
+
+def validate_rdf(data_graph: str, shacl_graph: str):
+
+    conforms, _, result_text = validate(data_graph,
+                                        data_graph_format="ttl",
+                                        shacl_graph=shacl_graph,
+                                        shacl_graph_format="ttl",
+                                        inference="rdfs",
+                                        advanced=True,
+                                        debug=False)
+
+    return {"conforms": conforms,
+            "validationReport": result_text}
+
+
+# Testing
+SHAPES_DIR = "shapes"
+
+data_graph = "https://www.govdata.de/ckan/dataset/supermarkte.rdf"
+shacl_graph = os.path.join(SHAPES_DIR, "dcat-hvd.ttl")
+
+result = validate_rdf(data_graph, shacl_graph)
+
+print(result["validationReport"])
