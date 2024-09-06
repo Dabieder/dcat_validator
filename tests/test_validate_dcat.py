c4e2ab451334b6e2996c1cb81c0ba5e21fe80434
Preuß, Nils <nils.preuss@fst.tu-darmstadt.de>
2024-09-06 16:12:06 +0200

    moved testing script to test_validate_dcat

--- tests/test_validate_dcat.py
+++ tests/test_validate_dcat.py
@@ -1,5 +1,30 @@
+import os
+import requests
 from validation.validate_dcat import validate_rdf
 
+
 def test_validate_rdf():
-    validation_result = validate_rdf("rdf_data", "shape_file")
-    assert validation_result == True
+    SHAPES_DIR = "shapes"
+
+    url = "https://www.govdata.de/ckan/dataset/supermarkte.rdf"
+    filename = url.split("/")[-1]
+    filepath = f"./{filename}"
+
+    with open(filepath, "wb") as f:  # write/truncate, binary mode
+        content = requests.get(url, stream=True).content
+        f.write(content)  # writing / testing temporary file is probably unneccessary
+
+    data_graphs = {"url": url,
+                   "file": filepath,  # writing / testing temporary file is probably unneccessary
+                   "text": content}
+
+    shacl_graph = os.path.join(SHAPES_DIR, "dcat-ap_2.1.1_shacl_shapes.ttl")
+    ont_graph = None  # os.path.join(SHAPES_DIR, "dcat-ap-de-imports.ttl")
+
+    for label, data_graph in data_graphs.items():
+        print(f"Testing '{label}':")
+        conforms, result = validate_rdf(data_graph, shacl_graph, ont_graph)
+        print(conforms, result)
+        assert conforms is False
+
+    os.remove(filepath)
--- validation/validate_dcat.py
+++ validation/validate_dcat.py
@@ -1,5 +1,3 @@
-import os
-import requests
 from pyshacl import validate
 
 
@@ -18,29 +16,3 @@ def validate_rdf(data_graph: str, shacl_graph: str, ont_graph: str):
                                         debug=False)
 
     return conforms, result_text
-
-
-# Testing
-SHAPES_DIR = "shapes"
-
-url = "https://www.govdata.de/ckan/dataset/supermarkte.rdf"
-filename = url.split("/")[-1]
-filepath = f"./validation/{filename}"
-
-with open(filepath, "wb") as f:  # write/truncate, binary mode
-    content = requests.get(url, stream=True).content
-    f.write(content)
-
-data_graphs = {"url": url,
-               "file": filepath,
-               "text": content}
-
-shacl_graph = os.path.join(SHAPES_DIR, "dcat-ap_2.1.1_shacl_shapes.ttl")
-ont_graph = None  # os.path.join(SHAPES_DIR, "dcat-ap-de-imports.ttl")
-
-for label, data_graph in data_graphs.items():
-    print(f"Testing '{label}':")
-    conforms, result = validate_rdf(data_graph, shacl_graph, ont_graph)
-    print(conforms, result)
-
-os.remove(filepath)
