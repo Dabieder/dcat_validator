9477d81e35382257681ae8d0660c3bb01dea4813
Preuß, Nils <nils.preuss@fst.tu-darmstadt.de>
2024-09-10 01:27:19 +0200

    update test_validate_dcat with vocabulary import

--- tests/test_validate_dcat.py
+++ tests/test_validate_dcat.py
@@ -1,6 +1,17 @@
 import os
 import requests
+from pyshacl.rdfutil import load_from_source, mix_graphs
+from rdflib import Graph
 from validation.validate_dcat import validate_rdf
+
+
+def test_vocab_import():
+    SHAPES_DIR = "shapes"
+    vocab_graph = os.path.join(SHAPES_DIR, "dcat-ap-de-imports.ttl")
+    g = load_from_source(vocab_graph, do_owl_imports=True)
+    print(g.serialize(format="ttl"))
+    assert isinstance(g, Graph)
+    # add assertions for sample statements the imported graph should contain
 
 
 def test_validate_rdf():
@@ -18,12 +29,18 @@ def test_validate_rdf():
                    "file": filepath,  # writing / testing temporary file is probably unneccessary
                    "text": content}
 
-    shacl_graph = os.path.join(SHAPES_DIR, "dcat-ap_2.1.1_shacl_shapes.ttl")
-    ont_graph = None  # os.path.join(SHAPES_DIR, "dcat-ap-de-imports.ttl")
+    shacl_graph = os.path.join(SHAPES_DIR, "dcat-ap-de.ttl")
+    ont_graph = os.path.join(SHAPES_DIR, "dcat-ap-de-imports.ttl")
+    ont_graph = load_from_source(ont_graph, do_owl_imports=True)
 
     for label, data_graph in data_graphs.items():
         print(f"Testing '{label}':")
-        conforms, result = validate_rdf(data_graph, shacl_graph, ont_graph)
+        if not isinstance(data_graph, Graph):
+            original_data_graph = load_from_source(data_graph, do_owl_imports=False)
+
+        data_graph = mix_graphs(original_data_graph, ont_graph)
+
+        conforms, result = validate_rdf(data_graph, shacl_graph, None)
         print(conforms, result)
         assert conforms is False
 
