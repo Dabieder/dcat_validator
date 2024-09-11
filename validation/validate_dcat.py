57ebbafad7a7fe000d582cfcf7bebb99a60ffca5
Daniel Biedermann <dbnakkiel@gmail.com>
2024-09-11 07:44:05 +0200

    make data_format a parameter

--- validation/validate_dcat.py
+++ validation/validate_dcat.py
@@ -1,11 +1,11 @@
 from pyshacl import validate
 
 
-def validate_rdf(data_graph: str, shacl_graph: str, ont_graph: str):
+def validate_rdf(data_graph: str, shacl_graph: str, ont_graph: str, data_graph_format: str = "xml"):
 
     print(f"{data_graph}")
     conforms, _, result_text = validate(data_graph,
-                                        data_graph_format="xml",
+                                        data_graph_format=data_graph_format,
                                         shacl_graph=shacl_graph,
                                         shacl_graph_format="ttl",
                                         ont_graph=ont_graph,
